0e57ca7f07fb20b4127e57c3e92ab9fcc2673a3c
DJ_Kovrik <djkovrik@gmail.com>
2017-05-19 16:48:36 +0300

    Add PreferencesHelper

--- XYZReader/src/main/java/com/sedsoftware/xyzreader/data/DataManager.java
+++ XYZReader/src/main/java/com/sedsoftware/xyzreader/data/DataManager.java
@@ -1,6 +1,7 @@
 package com.sedsoftware.xyzreader.data;
 
 import com.sedsoftware.xyzreader.data.local.DatabaseHelper;
+import com.sedsoftware.xyzreader.data.local.PreferencesHelper;
 import com.sedsoftware.xyzreader.data.model.Article;
 import com.sedsoftware.xyzreader.data.remote.ArticlesService;
 import io.reactivex.Observable;
@@ -12,11 +13,15 @@ public class DataManager {
 
   private final ArticlesService articlesService;
   private final DatabaseHelper databaseHelper;
+  private final PreferencesHelper preferencesHelper;
 
   @Inject
-  public DataManager(ArticlesService articlesService, DatabaseHelper databaseHelper) {
+  public DataManager(ArticlesService articlesService, DatabaseHelper databaseHelper,
+      PreferencesHelper preferencesHelper) {
+
     this.articlesService = articlesService;
     this.databaseHelper = databaseHelper;
+    this.preferencesHelper = preferencesHelper;
   }
 
   public Observable<Article> loadArticlesRemote() {
--- XYZReader/src/main/java/com/sedsoftware/xyzreader/injection/components/ApplicationComponent.java
+++ XYZReader/src/main/java/com/sedsoftware/xyzreader/injection/components/ApplicationComponent.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import com.sedsoftware.xyzreader.XYZReaderApp;
 import com.sedsoftware.xyzreader.data.DataManager;
 import com.sedsoftware.xyzreader.data.local.DatabaseHelper;
+import com.sedsoftware.xyzreader.data.local.PreferencesHelper;
 import com.sedsoftware.xyzreader.injection.ApplicationContext;
 import com.sedsoftware.xyzreader.injection.modules.ApplicationModule;
 import dagger.Component;
@@ -24,4 +25,6 @@ public interface ApplicationComponent {
   DataManager getDataManager();
 
   DatabaseHelper getDatabaseHelper();
+
+  PreferencesHelper getPreferencesHelper();
 }
